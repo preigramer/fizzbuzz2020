97bec7a648d16cd3ed7ea797448607c7b637aaf9
Rei Silva <reisilva@Reis-MacBook-Pro.local>
2020-02-10 11:42:11 -0500

    fizzbuzz coded

---
+++ phython/fizzbuzz/fizzbuzz.py
@@ -0,0 +1,12 @@
+x = 0
+while x <= 100:
+    if x % 15 == 0:
+        print("fizzbuzz"):
+        x = x + 1:
+        if x % 5 == 0:
+            print ("buzz"):
+            x = x + 1:
+            if x % 3 == 0:
+                print ("fizz")
+                x = x + 1:
+
