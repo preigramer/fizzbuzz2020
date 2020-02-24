1dc726a5cce756e89bc154201b4d25d1eb67c379
Rei Silva <reisilva@Reis-MacBook-Pro.local>
2020-02-24 11:47:12 -0500

    made functions for fizz,buzz, and fizzbuzz

--- phython/fizzbuzz/fizzbuzz_test.py
+++ phython/fizzbuzz/fizzbuzz_test.py
@@ -1,17 +1,23 @@
 
-def throws_an_error_if_no_input():
-    with raises(Exception) as err_info:
-        fizz()
-    assert err_info.type
+
+from fizzbuzz import Fizz, Buzz, Fibu
+
+def describes_a_buzz_function(): ""
+
+    def throws_an_error_if_no_input():
+        with raises(Exception) as err_info:
+        buzz()
+    assert err_info == TypeError
+    assert 'missing 1 requiried positional argument' in s
 
 
-def can_determine_if_a_number_is_a_multiple_of_3():
-    """Checks to see if a given number is a multiple of 3."""
-    assert fizz(3)== 'Fizz'   #multiple of 3
-    assert fizz(2)== 2 #non-multiple of 3
-    assert fizz(3.5)== 3.5 #non - integer
-    assert fizz(0)== 0   #zero
-    assert fizz(-3)== 'Fizz'  #negative multiple of 3
-    assert fizz(-4)== -4 #negative non-multiple of 3
-    assert fizz("buzz")== 'buzz' #non-numeric input
-    assert fizz() == 'error' #no input
+    def can_determine_if_a_number_is_a_multiple_of_5():
+        """Checks to see if a given number is a multiple of 5."""
+        assert Buzz(5)== 'Buzz'         #multiple of 5
+        assert Buzz(2)== 2              #non-multiple of 5
+        assert Buzz(5.5)== 3.5          #non - integer
+        assert Buzz(0)== 0              #zero
+        assert Buzz(-5)== 'Buzz'        #negative multiple of 5
+        assert Buzz(-4)== -4            #negative non-multiple of 5
+        assert Buzz("Buzz")== 'Buzz'    #non-numeric input
+        assert Buzz() == 'error'        #no input
--- phython/fizzbuzz/realfizzbuzz.py
+++ phython/fizzbuzz/realfizzbuzz.py
@@ -4,13 +4,15 @@ from numbers import Number
 
 def fizz(x):
     """ 
-    Takes an input 'x' and checks to see if it is a number, and if so, also a multiple of 3.
+    Takes an input 'x' and checks to see if x is a number, 
+    and if so, also a multiple of 3.
     If it is both, return 'fizz', Otherwise, return the input.
     """
-    # determine if x is a number or not
-    if isinstance(x, Number) and x % 3 ==0:
-        # yes, it is a number
-            return 'fizz'
-        else:
-            # no it is not a number 
-            return x
+    return 'fizz' if isinstance(x,Number) and x % 3 == 0 else x
+    # # determine if x is a number or not
+    # if isinstance(x, Number) and x % 3 == 0:
+    #     # yes, it is a number
+    #         return 'fizz'
+    #     else:
+    #         # no it is not a number 
+    #         return x
